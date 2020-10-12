75b2d61e4eff7b34495e012c27916db35b41d23f
Matt <visionsofparadise@pm.me>
2020-10-12 23:32:13 +0100

    authorizeUri prop

--- src/cdk/ApiKeyAuthorizer.ts
+++ src/cdk/ApiKeyAuthorizer.ts
@@ -1,6 +1,5 @@
 import { AuthorizationType, CfnAuthorizer, CfnAuthorizerProps, TokenAuthorizer } from '@aws-cdk/aws-apigateway';
-import { Function, IFunction } from '@aws-cdk/aws-lambda';
-import { Construct, Stack } from '@aws-cdk/core';
+import { Construct } from '@aws-cdk/core';
 
 export class ApiKeyAuthorizer extends Construct {
 	public readonly authorizer: {
@@ -11,29 +10,15 @@ export class ApiKeyAuthorizer extends Construct {
 	constructor(
 		scope: Construct,
 		id: string,
-		props: Pick<CfnAuthorizerProps, 'restApiId'> & { functionArn: string; cacheTtl?: number } & Partial<
-				CfnAuthorizerProps
-			>
+		props: Pick<CfnAuthorizerProps, 'restApiId'> & { authorizeUri: string } & Partial<CfnAuthorizerProps>
 	) {
 		super(scope, id);
-
-		const lambdaAuthorizerArn = (handler: IFunction) => {
-			return `arn:${Stack.of(handler).partition}:apigateway:${
-				Stack.of(handler).region
-			}:lambda:path/2015-03-31/functions/${handler.functionArn}/invocations`;
-		};
-
-		const apiKeyAuthorizerHandler = Function.fromFunctionArn(
-			this,
-			'apiKeyAuthorizerFunctionFromArn',
-			props.functionArn
-		);
 
 		const apiKeyAuthorizer = new CfnAuthorizer(this, 'tokenAuthorizer', {
 			name: id,
 			restApiId: props.restApiId,
 			type: 'TOKEN',
-			authorizerUri: lambdaAuthorizerArn(apiKeyAuthorizerHandler),
+			authorizerUri: props.authorizeUri,
 			identitySource: 'method.request.header.Authorization',
 			authorizerResultTtlInSeconds: 300
 		});
