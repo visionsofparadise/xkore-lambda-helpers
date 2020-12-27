376091ba3401967fa06aa31db31c17fa291c8369
Matt <visionsofparadise@pm.me>
2020-12-27 04:06:14 +0000

    fix

--- src/putEvents.ts
+++ src/putEvents.ts
@@ -1,24 +1,20 @@
 import { EventBridge } from 'aws-sdk';
 import { logger } from './logger';
 
-export const createPutEvents = (eventbridge: EventBridge, source: string) => async (
-	events: Array<{ detailType: string; detail: object }>
-) => putEvents(eventbridge, source, events);
-
-export const putEvents = async (
-	eventbridge: EventBridge,
-	source: string,
-	events: Array<{ detailType: string; detail: object }>
-) => {
+export const putEvents = async (eventbridge: EventBridge, source: string, events: Array<{ 0: string; 1: object }>) => {
 	logger.info({ events });
 
 	return eventbridge
 		.putEvents({
 			Entries: events.map(event => ({
 				Source: source,
-				DetailType: event.detailType,
-				Detail: JSON.stringify(event.detail)
+				DetailType: event[0],
+				Detail: JSON.stringify(event[1])
 			}))
 		})
 		.promise();
 };
+
+export const createPutEvents = (eventbridge: EventBridge, source: string) => async (
+	events: Array<{ 0: string; 1: object }>
+) => putEvents(eventbridge, source, events);
