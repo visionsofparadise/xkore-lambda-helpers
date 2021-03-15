3e14a36d01160346dc75a850ce2e757c7a9b3e0c
Matt <visionsofparadise@pm.me>
2021-03-15 21:30:33 +0000

    Revert "fix item resource"
    This reverts commit 4f1c6740a25001fd4db72ba37ffea4f39a743046.

--- src/cdk/ItemResource.ts
+++ src/cdk/ItemResource.ts
@@ -1,4 +1,5 @@
 import { Construct, Stack } from '@aws-cdk/core';
+import { Item as ItemClass } from '../Item';
 import { IDocumentation, Documentation } from '../Documentation';
 import { Documented } from './DocumentationItems';
 
@@ -6,17 +7,10 @@ export interface ItemResourceProps {
 	tags?: Array<string>;
 }
 
-interface ItemClass {
-	jsonSchema: object;
-	hiddenKeys: Array<string>;
-	ownerKeys: Array<string>;
-	tags: Array<string>;
-}
-
 export class ItemResource extends Construct implements Documented {
-	public Item: ItemClass;
+	public Item: typeof ItemClass;
 
-	constructor(scope: Construct, Item: ItemClass, props?: ItemResourceProps) {
+	constructor(scope: Construct, Item: typeof ItemClass, props?: ItemResourceProps) {
 		super(scope, (Item.jsonSchema as { title: string }).title!);
 
 		this.Item = Item;
